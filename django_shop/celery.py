a22f58f385990908c97648effd8e900dfb6947aa
Dmitry <od@od7.ru>
2020-07-23 12:39:25 +0500

    load project

---
+++ django_shop/celery.py
@@ -0,0 +1,10 @@
+import os
+from celery import Celery
+
+
+# Задаем переменную окружения,
+# содержащую название файла настроек нашего проекта.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_shop.settings')
+app = Celery('django_shop')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
